e25a2b6157f1d69066af733aa37575619bb4b56a
real-future <future.ver1@gmail.com>
2023-08-28 18:32:52 +0900

    init

---
+++ DayByDay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DayByDay
+//
+//  Created by FUTURE on 2023/08/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
